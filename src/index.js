b6c08f6287f62edf51d06e7478e861f75e8d213d
Vatsal Gupta <67777735+MrNenoo@users.noreply.github.com>
2023-04-12 13:14:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Signup from './Signup';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Signup />
+    <App />
   </React.StrictMode>
 );
 
